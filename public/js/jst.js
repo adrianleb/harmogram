169dffbecf5f61a0ce2ce52884a495fb19620986
Adrian le Bas <adrianleb@gmail.com>
2013-09-12 19:52:56 +0200

    dev up

--- public/js/jst.js
+++ public/js/jst.js
@@ -86,7 +86,6 @@ JST['channel'] = function(__obj) {
   }
   (function() {
     (function() {
-    
       __out.push('  <div class="track__image" style="background-image:url(');
     
       __out.push(__sanitize(this.model.get('image')));
@@ -249,7 +248,6 @@ JST['controls'] = function(__obj) {
   }
   (function() {
     (function() {
-    
       __out.push('\n<ul class="controls__wrap">\n  <li class="controls__button" > \n    <label for="control-smooth">Fullscreen</label>\n    <input type="checkbox" id="control-fullscreen">\n  </li>\n  <li class="controls__button" > \n    <label for="control-smooth">Centered</label>\n    <input type="checkbox" id="control-centered">\n  </li>\n  <li class="controls__button" > \n    <label for="control-smooth">Smooth</label>\n    <input type="checkbox" id="control-smooth" data-control-switch="smooth">\n  </li>\n  <li class="controls__range">\n    <label for="control-step">Layer</label>\n    <input type="range" data-control-method="gapper" class="" id="control-steps" min=\'1\' max=\'3\' value="1">\n  </li>\n    \n  <li class="controls__section ">\n    <label class="controls__sectitle" for="control-radius">Angle:</label>\n    <div class="controls__button controls--hideswitch" >\n      <label for="control-changeangle">Dynamic Angle</label>\n      <input data-control-switch="changeAngle" type="checkbox" id="control-changeangle">\n    </div>\n    <div class="controls__range controls--hided">\n      <label for="control-angle">Angle Mod Speed</label>\n      <input type="range" data-control-method-value="1" data-control-method="changeAngleSpeed" class="" id="control-anglespeed" min=\'-100\' max=\'100\' value="0">\n    </div>\n    <div class="controls__range controls--tohide">\n      <label for="control-angle">Angle</label>\n      <input type="range" data-control-method-value="1" data-control-method="baseAngle" class="" id="control-angle" min=\'-1000\' max=\'1000\' value="1">\n    </div>\n  </li>\n  <li class="controls__section controls__section--inliner">\n    <label class="controls__sectitle" for="control-radius">Radius:</label>\n    <div class="controls__button controls--hideswitch" >\n      <label for="control-radius-rand">Dynamic Radius</label>\n      <input data-control-switch="changeRadius" type="checkbox" id="control-radius-rand">\n    </div>\n    <div class="controls__range  controls--hided">\n      <label for="control-radius ">Min Amp</label>\n      <input type="range" data-control-method="radiusAmp" data-control-method-value="1" class="" id="control-radius" min=\'-100\' max=\'100\' value="1">\n    </div>\n    <div class="controls__range controls--tohide">\n      <label for="control-radius ">Min</label>\n      <input type="range" data-control-method-value="1" data-control-method="baseRadius" class="" id="control-radius" min=\'-300\' max=\'100\' value="50">\n    </div>\n    <div class="controls__range">\n      <label for="control-amp">Max</label>\n      <input type="range" data-control-method-value="1" data-control-method="ampVal" class="" id="control-amp" min=\'-30\' max=\'30\' value="5">\n    </div>\n  </li>\n\n  \n  <li class="controls__section ">\n    <label class="controls__sectitle" for="control-radius">Color:</label>\n    <div class="controls__button controls--hideswitch" >\n      <label for="control-color-active">Color Path</label>\n      <input data-control-switch="paintAmp" type="checkbox" id="control-color-active">\n    </div>\n    <div class="controls__button controls--hideswitch" >\n      <label for="control-colorbg">Color Background</label>\n      <input data-control-switch="paintBg" type="checkbox" id="control-colorbg">\n    </div>\n    <div class="controls__button controls--hided" >\n      <label for="control-colorbg">Invert</label>\n      <input data-control-switch="colorInvert" type="checkbox" id="control-colorbg">\n    </div>\n    <div class="controls__range controls--hided">\n      <label for="control-hue">Hue</label>\n      <input type="range" data-control-method-value="1" data-control-method="hue" class="" id="control-hue" min=\'0\' max=\'255\' value="42">\n    </div>\n    <div class="controls__range controls--hided">\n      <label for="control-hue">Saturation</label>\n      <input type="range" data-control-method-value="1" data-control-method="colorSat" id="control-hue" min=\'0\' max=\'255\' value="7">\n    </div>\n    <div class="controls__range controls--hided">\n      <label for="control-hue">lightness Amp</label>\n      <input type="range" data-control-method-value="1" data-control-method="colorAmp" id="control-hue" min=\'0\' max=\'300\' value="66">\n    </div>\n    <div class="controls__button controls--hided" >\n      <label for="control-huechanger">Hue Modulation</label>\n      <input data-control-switch="changeHue" type="checkbox" id="control-huechanger">\n    </div>\n  </li>\n\n  \n  <!-- <li class="controls__range"><label for="control-baseoffset">Freq</label><input type="range" data-control-method="baseOffset" class="" id="control-baseoffset" min=\'0\' max=\'300\' value="200"></li> -->\n\n</ul>');
     
     }).call(this);
@@ -297,7 +295,6 @@ JST['customTrack'] = function(__obj) {
   }
   (function() {
     (function() {
-    
       console.log(this.url, this.index);
     
       __out.push('\n  <div class="track__image">\n  </div>\n  <div class="track__info">\n    <h1 class="track__artist">NN</h1>\n    <h2 class="track__title">NN</h2>\n  </div>\n  <audio id="source');
@@ -359,7 +356,6 @@ JST['home'] = function(__obj) {
   }
   (function() {
     (function() {
-    
     
     
     }).call(this);
